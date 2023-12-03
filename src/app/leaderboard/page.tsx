24206b9f9898479b5ff369acf3f086450d7b85d7
cjuillard <colin.juillard@gmail.com>
2023-12-03 12:50:41 -0700

    Style the leaderboard page

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -5,22 +5,41 @@ export default async function Leaderboard() {
 
   const userTableItems = users.map((user, i) => {
     return (
-      <tr key={i}>
-        <td>{user.name}</td>
-        <td>{user.coins}</td>
+      <tr
+        key={i}
+        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+      >
+        <td
+          scope="row"
+          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        >
+          {i + 1}
+        </td>
+        <td
+          scope="row"
+          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        >
+          {user.name}
+        </td>
+        <td className="px-6 py-2">💰{user.coins}</td>
       </tr>
     );
   });
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th>Leaderboard</th>
-          </tr>
-        </thead>
-        <tbody>{userTableItems}</tbody>
-      </table>
+      <center>
+        <h1 className="py-5 text-3xl">🥇Leaderboard🥇</h1>
+        <table className="w-1/2 text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 shadow-md">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th className="px-6 py-3 w-2/12">Ranking</th>
+              <th className="px-6 py-3">Name</th>
+              <th className="px-6 py-3">Coins</th>
+            </tr>
+          </thead>
+          <tbody>{userTableItems}</tbody>
+        </table>
+      </center>
     </>
   );
 }
